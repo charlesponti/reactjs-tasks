892fe8e00290adc753d37f050d38f3aa5c453c46
CJ Ponti <cjponti@gmail.com>
2015-01-25 19:40:57 +0000

    Add development js and css

--- dist/semantic-nav.js
+++ dist/semantic-nav.js
@@ -1,61 +1,41 @@
+'use strict';
+
+/**
+ * @desc Add event listeners to elements for site navigation controls
+ */
 (function() {
-  /**
-   * @desc <header> element
-   * @type {HTMLElement}
-   */
-  var element = document.querySelectorAll('header')[0];
+  var querySelector = document.querySelector.bind(document);
+
+  var main = querySelector('main');
+  var header = querySelector('.site-header');
+  var navBtn = querySelector('.site-header .menu');
+  var siteNav = querySelector('.site-nav');
 
   /**
-   * @desc Find elements within <header>
-   * @param {String} selector
-   * @return {HTMLElement[]}
+   * @desc Remove .open class from <main>, .site-header, and .site-nav
    */
-  element.find = function(selector) {
-    return [].slice.call(element.querySelectorAll.call(element, selector));
-  };
-
-  /**
-   * @desc Attach function to event on HTMLElement
-   * @param {HTMLElement[]} nodes
-   * @param {String} event Event to attach function to
-   * @param {Function} fn Function to exectue when event occurs
-   * @return {HTMLElement[]} nodes
-   */
-  var on = function(nodes, event, fn) {
-    return nodes.forEach(function(node) {
-      node[event] = fn;
+  function closeMenu() {
+    return [main, header, siteNav, navBtn].forEach(function(el) {
+      return el.classList.remove('open');
     });
-  };
-
-  var list = element.find('ul')[0];
-  // var listItems = element.find('li');
-  var links = element.find('a');
-  var toggle = element.find('button');
-
-  /**
-   * @desc Toggle .open class on <ul> within <header>
-   */
-  function toggleList() {
-      return list.classList.toggle('open');
   }
 
-  // Toggle 'open' class when mouse leaves <nav>
-  on([element], 'onmouseleave', function() {
-    // Only toggle class if list has class
-    if (list.classList.contains('open')) {
-      return list.classList.remove('open');
+  /**
+   * @desc Toggle .open class on <main>, .site-header, and .site-nav
+   */
+  function toggleMenu() {
+    return [main, header, siteNav, navBtn].forEach(function(el) {
+      return el.classList.toggle('open');
+    });
+  }
+
+  // Add 'click' event listeners to <main>, .site-header > .menu, and .site-nav
+  main.addEventListener('click', closeMenu);
+  navBtn.addEventListener('click', toggleMenu);
+  siteNav.addEventListener('click', function(event) {
+    if (/LI|A/.test(event.target.nodeName)) {
+      return closeMenu();
     }
     return;
   });
-
-  // Toggle 'open' class when link is clicked
-  on(links, 'onclick', toggleList);
-
-  // Toggle 'open' class when mouse leaves list
-  on([list], 'onmouseleave', toggleList);
-
-  // Toggle 'open' class when Menu button is clicked
-  on(toggle, 'onclick', toggleList);
-
-  return;
 })();
