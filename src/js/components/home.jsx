d654f44ee937ad0bbaa3d5fdcfc9b2cf653f8290
hackefeller <hackefeller@icloud.com>
2015-03-31 22:18:44 +0100

    Update h2 text in home component

--- src/js/components/home.jsx
+++ src/js/components/home.jsx
@@ -8,7 +8,7 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <h2 className="text-center"> Facade React </h2>
+        <h2 className="text-center"> React Boilerplate </h2>
         <div className="container" id="app"></div>
       </div>
     )
