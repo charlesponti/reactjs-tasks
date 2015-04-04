a6f55b58910bf3dbd647e1c53a1dddc91cdf8dd6
hackefeller <hackefeller@icloud.com>
2015-04-04 11:14:08 +0100

    Attach router to document in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,12 @@
 'use strict';
 
-// Require application
-import './app';
+import App from './app';
+import React from 'react';
+import Router from 'react-router';
+
+Router.run(App.routes, function (Handler) {
+  React.render(<Handler/>, document.querySelector('body'));
+});
 
 // Or, if you'd like to use the HTML5 history API for cleaner URLs:
 // Router.run(routes, Router.HistoryLocation, function (Handler) {
