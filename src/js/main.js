5d446990c179adefb0917d5f51e01100609c9947
hackefeller <hackefeller@icloud.com>
2015-04-06 11:38:27 +0100

    Render app to #app instead of body

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import React from 'react';
 import Router from 'react-router';
 
 Router.run(App.routes, function (Handler) {
-  React.render(<Handler/>, document.querySelector('body'));
+  React.render(<Handler/>, document.querySelector('#app'));
 });
 
 // Or, if you'd like to use the HTML5 history API for cleaner URLs:
