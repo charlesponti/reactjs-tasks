4249675393991aa35f94b7cc699e26ec65e50590
hackefeller <hackefeller@icloud.com>
2015-04-06 11:37:42 +0100

    Add constants to app/index.js

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,11 +1,14 @@
 'use strict';
 
 import dispatcher from './dispatcher';
+import constants from './constants';
 import routes from './routes';
 
 export default {
 
   dispatcher: dispatcher,
+
+  constants: constants,
 
   routes: routes
 
