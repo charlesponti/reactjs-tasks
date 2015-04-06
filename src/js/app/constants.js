a0b8e901e5a8faa6828283a516529b27500037c3
hackefeller <hackefeller@icloud.com>
2015-04-06 11:37:31 +0100

    Add app/constants.js

---
+++ src/js/app/constants.js
@@ -0,0 +1,15 @@
+'use strict';
+
+export default {
+
+  TASKS: {
+    COMPLETE: 'task-complete',
+    CREATE: 'task-create',
+    DESTROY: 'task-destroy',
+    TOGGLE_COMPLETE_ALL: 'tasks-complete-all',
+    UPDATE: 'task-update',
+    UNDO_COMPLETE: 'task-undo-complete'
+  }
+
+}
+
