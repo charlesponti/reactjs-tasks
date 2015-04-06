39ef3b168516eef65621400caacf38599e0d449f
hackefeller <hackefeller@icloud.com>
2015-04-06 11:38:11 +0100

    Remove header, nav, and main from pages/app.js

--- src/js/pages/app.js
+++ src/js/pages/app.js
@@ -1,55 +1,19 @@
 'use strict';
 
-import classnames from 'classnames';
 import React from 'react';
 import {RouteHandler} from 'react-router';
 
 class App extends React.Component {
 
   constructor() {
-    this.state = {
-      open: false
-    };
-  }
-
-  /**
-   * Handle click event
-   * @param {string} source
-   * @param {SyntheticEvent} event
-   */
-  onClick(source, event) {
-    // Always close nav drawer if main was clicked
-    if (source === 'main')
-      this.setState({ open: false });
-    // Otherwise toggle open
-    else
-      this.setState({ open: !this.state.open });
+    super();
   }
 
   render() {
-    let open = { open: this.state.open };
     return (
       <div>
-        <header className="site ui-blue">
-            <div
-              className={classnames("hamburger menu", open)}
-              href="#"
-              onClick={this.onClick.bind(this, 'icon')}>
-              <i></i>
-            </div>
-            <div className="logo">React:Life</div>
-        </header>
-        <nav className={classnames('site ui-blue', open)} onClick={this.onClick.bind(this, 'nav')}>
-          <ul>
-            <li>
-              <a href="#"> Link </a>
-            </li>
-          </ul>
-        </nav>
-        <main className={classnames(open)} onClick={this.onClick.bind(this, 'main')}>
-            {/* this is the important part */}
-            <RouteHandler/>
-        </main>
+        {/* this is the important part */}
+        <RouteHandler/>
       </div>
     )
   }
