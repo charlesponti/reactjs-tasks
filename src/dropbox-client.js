f1e0217658a296026062aa687c44d162afdabf73
hackefeller <hackefeller@icloud.com>
2015-04-11 20:23:47 +0100

    Rename client to dropbox

--- src/dropbox-client.js
+++ src/dropbox-client.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const client = new Dropbox.Client({key: 'w2ncihown3ze0at' });
+const dropbox = new Dropbox.Client({key: 'w2ncihown3ze0at' });
 
 // Try to finish OAuth authorization.
-client.authenticate({interactive: false}, function (error) {
+dropbox.authenticate({interactive: false}, function (error) {
   if (error) {
     alert('Authentication error: ' + error);
   }
 });
 
-export default client
+export default dropbox;
