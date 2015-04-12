1752002308fc47567a72507ab34c8aa10062b819
hackefeller <hackefeller@icloud.com>
2015-04-12 22:02:34 +0100

    Add getTable function to dropbox-client

--- src/dropbox-client.js
+++ src/dropbox-client.js
@@ -10,3 +10,21 @@ dropbox.authenticate({interactive: false}, function (error) {
 });
 
 export default dropbox;
+  /**
+   * Retrieve table from datastore
+   * @param {string} tableName
+   * @returns {Promise}
+   */
+  getTable: function(tableName) {
+    const datastoreManager = this.manager();
+    return new Promise((resolve, reject) => {
+      datastoreManager.openDefaultDatastore(function(error, store) {
+        if (error) {
+          return reject(error);
+        }
+
+        // Return table from datastore
+        return resolve(store.getTable(tableName));
+      });
+    });
+  }
