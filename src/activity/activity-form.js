ecb4dc9e15d74a70aa780c3cb558b96f15134639
hackefeller <hackefeller@icloud.com>
2015-04-12 17:44:02 +0100

    Add input#text and button to ActivityForm

--- src/activity/activity-form.js
+++ src/activity/activity-form.js
@@ -4,9 +4,16 @@ const React = require('react');
 
 module.exports = React.createClass({
 
+  onSubmit(event) {
+    event.preventDefault();
+
+  },
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
+        <input type="text" name="activity.name" ref="name" />
+        <button> Start Tracking </button>
       </form>
     )
   }
