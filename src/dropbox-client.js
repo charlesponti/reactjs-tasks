c269a0c13fc5c022658fb6dff509cbb78b1abd27
hackefeller <hackefeller@icloud.com>
2015-04-12 22:02:59 +0100

    Update dropbox-client

--- src/dropbox-client.js
+++ src/dropbox-client.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const dropbox = new Dropbox.Client({key: 'w2ncihown3ze0at' });
+const client = new Dropbox.Client({key: 'w2ncihown3ze0at' });
 
 // Try to finish OAuth authorization.
-dropbox.authenticate({interactive: false}, function (error) {
+client.authenticate({interactive: false}, function (error) {
   if (error) {
     alert('Authentication error: ' + error);
   }
 });
 
-export default dropbox;
+module.exports = {
+
+  client: client,
+
   manager: function() {
     if (client.isAuthenticated()) {
       return client.getDatastoreManager();
