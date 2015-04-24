489b6ac2a86770d2aa941f88ebf5be7a91887ed3
hackefeller <hackefeller@icloud.com>
2015-04-24 15:59:31 +0100

    Add edit and delete buttons

--- src/tasks/task-list.js
+++ src/tasks/task-list.js
@@ -4,7 +4,15 @@ import React from 'react';
 
 export default React.createClass({
 
-  render: function() {
+  onEditClick() {
+
+  },
+
+  onDeleteClick() {
+
+  },
+
+  render() {
     return (
       <ul className="task-list">
         {this.props.tasks.map((task) => {
@@ -12,6 +20,10 @@ export default React.createClass({
             <li key={task.id} className="task-list-item card">
               <input type="checkbox" value={task.completed} />
               <span>{task.description}</span>
+              <div className="pull-right">
+                <i className="fa fa-edit" onClick={this.onEditClick}></i>
+                <i className="fa fa-close" onClick={this.onDeleteClick}></i>
+              </div>
             </li>
           );
         })}
