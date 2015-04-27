5d6c7fce7e5f3b953205e471b36d5daf7d008a1a
hackefeller <hackefeller@icloud.com>
2015-04-27 15:51:59 +0100

    Add whitespace

--- src/firebase-client.js
+++ src/firebase-client.js
@@ -7,4 +7,5 @@ let client = new Firebase('https://ex5iofp0tf0.firebaseio-demo.com/');
 client.on(constants.FIREBASE.ADDED, function(snapshot) {
   //We'll fill this in later.
 });
+
 export default client;
