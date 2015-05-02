ecfbe2ec5628ef98e1805e5738f30a223750e1b7
hackefeller <hackefeller@icloud.com>
2015-05-02 15:15:25 +0100

    feat(signup): Create sign-up.js Create sign-up.js which contains the React component used for rendering the sign up page.

---
+++ src/pages/sign-up.js
@@ -0,0 +1,18 @@
+'use strict';
+
+import React from 'react';
+
+const SignUp = React.createClass({
+
+  render() {
+    return (
+      <div>
+        <input type="email" placeholder=" Email Address" />
+        <button> Sign Up </button>
+      </div>
+    );
+  }
+
+});
+
+export default SignUp;
