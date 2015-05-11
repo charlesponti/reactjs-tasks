7e48b583a4585ba24f562758e9d8d5f48d0a013b
hackefeller <hackefeller@icloud.com>
2015-05-11 19:33:08 +0100

    Assign different routes dependent on if there is a current user

--- src/app/routes.js
+++ src/app/routes.js
@@ -6,22 +6,34 @@ import Home from '../pages/home';
 import NotFound from '../pages/not-found';
 import TaskPage from '../tasks';
 import ActivityPage from '../activity';
+import SignUpPage from '../pages/sign-up.js';
 
 // Dependencies
 import React from 'react';
 import Router from 'react-router';
 
+let routes;
 let Route  = Router.Route;
 let DefaultRoute = Router.DefaultRoute;
 let NotFoundRoute = Router.NotFoundRoute;
 
-var routes = (
-  <Route path="/" handler={App}>
-    <DefaultRoute handler={Home} />
-    <NotFoundRoute handler={NotFound}/>
-    <Route path="tasks" handler={TaskPage} />
-    <Route path="activity" handler={ActivityPage} />
-  </Route>
-);
+if (Parse.User.current()) {
+  routes = (
+    <Route path="/" handler={App}>
+      <DefaultRoute handler={Home}/>
+      <NotFoundRoute handler={NotFound}/>
+      <Route path="tasks" handler={TaskPage}/>
+      <Route path="activity" handler={ActivityPage}/>
+    </Route>
+  );
+}
+else {
+  routes = (
+    <Route path="/" handler={App}>
+      <DefaultRoute handler={SignUpPage} />
+      <NotFoundRoute handler={NotFound}/>
+    </Route>
+  );
+}
 
 export default routes;
