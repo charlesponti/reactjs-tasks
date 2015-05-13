d65aefeac982bcc91451af7031d4f422d876fdb5
hackefeller <hackefeller@icloud.com>
2015-05-13 22:09:43 +0100

    Add onSubmit function to SignUp component

--- src/pages/sign-up.js
+++ src/pages/sign-up.js
@@ -4,6 +4,28 @@ import React from 'react';
 
 const SignUp = React.createClass({
 
+  onSubmit() {
+    let email = this.refs.email.getDOMNode().value;
+    let password = this.refs.password.getDOMNode().value;
+    let passwordConfirmation = this.refs.passwordConfirmation.getDOMNode().value;
+    let allValues = username && password && passwordConfirmation;
+    let matchingPasswords = password === passwordConfirmation;
+
+    if (allValues && matchingPasswords) {
+      this.state.user.set('username', email);
+      this.state.user.set('password', password);
+
+      this.state.user.signUp(null, {
+        success: function(user) {
+          console.log(user);
+        },
+        error: function(user, error) {
+          // Show the error message somewhere and let the user try again.
+          alert("Error: " + error.code + " " + error.message);
+        }
+      });
+    }
+  },
   render() {
     return (
       <div>
