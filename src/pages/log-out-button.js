721a0fa0afe9d66a5455862801b48edcb4813a95
hackefeller <hackefeller@icloud.com>
2015-05-23 13:21:23 +0100

    Add log-out-button.js

--- src/pages/log-out-button.js
+++ src/pages/log-out-button.js
@@ -1,1 +1,22 @@
 'use strict';
+
+import React from 'react';
+
+const LogOut = React.createClass({
+
+  /**
+   * Log out user when button is clicked
+   */
+  onClick() {
+    return Parse.User.logOut();
+  },
+
+  render() {
+    return (
+      <button onClick={this.onClick}> Log Out </button>
+    );
+  }
+
+});
+
+export default LogOut;
