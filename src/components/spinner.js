53bc54a366d198e0ab749dbc77a636788481dc88
hackefeller <hackefeller@icloud.com>
2015-06-01 17:19:29 +0100

    Add spinner.js

--- src/components/spinner.js
+++ src/components/spinner.js
@@ -1,1 +1,24 @@
 'use strict';
+
+import React from 'react';
+
+const Spinner = React.createClass({
+
+  render() {
+    let klass;
+    let spin = this.props.spin || false;
+
+    klass = spin ? "fa fa-2x fa-refresh fa-spin" : "fa fa-2x fa-refresh";
+
+    if (this.props.show) {
+      return (
+        <i className={klass}></i>
+      );
+    }
+    else {
+      return (<span></span>);
+    }
+  }
+});
+
+export default Spinner;
