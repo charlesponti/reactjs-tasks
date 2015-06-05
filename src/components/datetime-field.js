6be046c095f6b3e354961686f733a78cb8b6ac15
hackefeller <hackefeller@icloud.com>
2015-06-05 19:21:52 +0100

    Add 'start date' and 'end date' fields to datetime-field.js

--- src/components/datetime-field.js
+++ src/components/datetime-field.js
@@ -2,11 +2,29 @@
 
 export default React.createClass({
 
+  onStartDateChange() {
+
+  },
+
+  onEndDateChange() {
+
+  },
+
   render() {
-    <fieldset>
-      <label> Date </label>
-      <input type="datetime-local" name={this.props.name}/>
-    </fieldset>
+    let fieldName = this.props.name;
+
+    return (
+      <div>
+        <fieldset>
+          <label> Start Date </label>
+          <input type="datetime-local" name={`${fieldName}[start]`}/>
+        </fieldset>
+        <fieldset>
+          <label> End Date </label>
+          <input type="datetime-local" name={`${fieldName}[end]`}/>
+        </fieldset>
+      </div>
+    );
   }
 
 })
