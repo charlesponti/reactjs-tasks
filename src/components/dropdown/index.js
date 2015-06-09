86786a37119ad748636da0cda2b091fb25ce5755
hackefeller <hackefeller@icloud.com>
2015-06-09 16:29:45 +0100

    Create dropdown module

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+import React from 'react';
+
+function plucker(key, obj) {
+  return key ? obj[key] : obj;
+}
+
 export default React.createClass({
 
   getInitialState() {
@@ -9,20 +15,20 @@ export default React.createClass({
   },
 
   selectItemByValue() {
-    if (value == null) return;
+    let pluck = this.props.pluck;
 
     let found = _.find(this.state.options, function(item) {
       let val;
 
       if (typeof item.value === 'object') {
-        val = window.plucker($scope.pluck, item.value).toLowerCase();
+        val = plucker(pluck, item.value);
       }
       else if (typeof item.value === 'string') {
         val = item.value.toLowerCase();
       }
 
       if (typeof value === 'object') {
-        return val === window.plucker($scope.pluck, value).toLowerCase();
+        return val === plucker(pluck, value);
       }
       else if (typeof value === 'string') {
         return  val === value.toLowerCase();
@@ -61,26 +67,20 @@ export default React.createClass({
     return options.map(function(item, index) {
       let type = typeof item;
 
+      let option = {
+        itemId: index,
+        value: item
+      };
+
       if (type === 'string') {
-        item = {
-          text: item,
-          value: item
-        };
-      }
-      else if (type === 'object' && pluck) {
-        item = {
-          text: window.plucker(pluck, item),
-          value: item
-        };
+        option.label = item;
       }
 
-      item.itemID = index;
-
-      if (pluck) {
-        item[pluck] = window.plucker(pluck, item.value);
+      if (type === 'object' && pluck) {
+        option.label = plucker(pluck, item);
       }
 
-      return item;
+      return option;
     });
   },
 
@@ -103,20 +103,32 @@ export default React.createClass({
     let spinner = (
       <i style="color: darkgray; font-size:20px" class="fa fa-refresh fa-spin"></i>
     );
+    let selected = this.state.selected;
 
     return (
-      <span>
-        {this.state.loadingItems ? spinner : <span></span>}
-        <select onChange={this.onChange}
-                name={this.props.name}
-                value={this.props.initial}>
-          {this.state.options.map((option, index) => {
-            return (
-              <option value={index} key={index}>{option.text}</option>
-            );
-          })}
-        </select>
-      </span>
+      <fieldset>
+        <label>{this.props.label}</label>
+        <div className="ui-dropdown">
+          {this.state.loadingItems ? spinner : <span></span>}
+          <select onChange={this.onChange}
+                  name={this.props.name}
+                  value={this.props.initial}>
+          </select>
+          <div className="ui-dropdown__selected">
+            {selected ? selected.label : ''}
+          </div>
+          <div className="ui-dropdown__options">
+            {this.state.options.map((option, index) => {
+              return (
+                <div key={index}>
+                  {option.label}
+                  <option value={index}></option>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </fieldset>
     );
   }
 
